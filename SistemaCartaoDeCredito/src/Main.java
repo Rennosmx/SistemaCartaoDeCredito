a92e21374ec8e4414030c6ec2a0eb8601e894043
Renno <rennosmx@hotmail.com>
2016-11-29 23:59:07 -0300

    Alteração do valor de crédito do CLiente

--- SistemaCartaoDeCredito/src/Main.java
+++ SistemaCartaoDeCredito/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
   public static void main(String[] args) {
-    CartaoCredito cartaoCliente = new CartaoCredito("Cliente", "1111", 20.0);
+    CartaoCredito cartaoCliente = new CartaoCredito("Cliente", "1111", 40.0);
     CartaoCredito cartaoProdutora = new CartaoCredito("Produtora", "2222", 0.0);
 
     Sistema.getInstance().getCartoes().add(cartaoCliente);
